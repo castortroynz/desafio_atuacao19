b9ccdc212685ce7d1ad1a4f3841c37d0246ddc27
Alysson Nishiyama <castortroynz@gmail.com>
2019-07-26 05:35:45 -0300

    up

--- desafio_atuacao19.py
+++ desafio_atuacao19.py
@@ -129,8 +129,8 @@ processor = [TokenizeProcessor(tokenizer=tokenizer), NumericalizeProcessor(max_v
 
 
 data_lm = (TextList.from_csv(path, 'treinamento_utf-8.csv', cols='mensagem', processor=processor)           
-            .split_by_rand_pct(0.1, seed=50)       #Dividindo randomicamente o dataset em 10% para validação
-            .label_for_lm()                        ##Setando a coluna de label
+            .split_by_rand_pct(0.1, seed=50) #Dividindo randomicamente o dataset em 10% para validação
+            .label_for_lm()                  ##Setando a coluna de label
             .databunch(bs=bs))
 data_lm.save('data_lm.pkl')
 
